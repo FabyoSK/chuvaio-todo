89d067cd85c6b4cc5fbacf3e0f0668f4e4de180b
Fábio Alves <fabioalves.contacto@gmail.com>
2021-03-27 07:58:47 -0100

    Feat: create TodosController to handle the CRUD operations

--- server/src/controllers/todoController.ts
+++ server/src/controllers/todoController.ts
@@ -1,7 +1,39 @@
-import db from "../database/connection";
+import { Request, Response } from "express";
+import { uuid } from "uuidv4";
 
-function get(req, res) {
-  const todos = db.select().from("todos");
+interface Todo {
+  id: string;
+  content: string;
+}
 
-  return res.json(todos);
+const todos = <Todo[]>[];
+
+export default class TodoController {
+  async list(request: Request, response: Response) {
+    return response.json(todos);
+  }
+
+  async create(request: Request, response: Response) {
+    const { content } = request.body;
+
+    const todo: Todo = { id: uuid(), content: content };
+
+    todos.push(todo);
+
+    return response.json(todo);
+  }
+
+  async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
+
+    if (todoIndex < 0) {
+      return response.status(400).json({ ERROR: "Todo not found." });
+    }
+
+    todos.splice(todoIndex, 1);
+
+    return response.status(204).send();
+  }
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,54 +1,13 @@
 import express from "express";
-import { uuid } from "uuidv4";
+import TodoController from "./controllers/todoController";
 
 const routes = express.Router();
+const todoController = new TodoController();
 
-interface Todo {
-  id: string;
-  content: string;
-}
+routes.get("/todos", todoController.list);
 
-const todos = <Todo[]>[];
+routes.post("/todos", todoController.create);
 
-routes.get("/todos", (req, res) => {
-  return res.json(todos);
-});
-
-routes.post("/todos", (req, res) => {
-  const { content } = req.body;
-
-  const todo: Todo = { id: uuid(), content: content };
-
-  todos.push(todo);
-
-  return res.json(todo);
-});
-
-// app.put("/todos/:id", (req, res) => {
-//   const { id } = req.params;
-//   const { content } = req.body;
-
-//   const todo = { id, content };
-
-//   const todoIndex = todos.findIndex((todo) => todo.id === id);
-//   if (todoIndex < 0) {
-//     return res.status(400).json({ ERROR: "Todo not found." });
-//   }
-//   todos[todoIndex] = todo;
-
-//   return res.json(todo);
-// });
-
-routes.delete("/todos/:id", (req, res) => {
-  const { id } = req.params;
-
-  const todoIndex = todos.findIndex((todo) => todo.id === id);
-  if (todoIndex < 0) {
-    return res.status(400).json({ ERROR: "Todo not found." });
-  }
-  todos.splice(todoIndex, 1);
-
-  return res.status(204).send();
-});
+routes.delete("/todos/:id", todoController.delete);
 
 export default routes;
